c49097cd51f93cb40265c78b85b27ad6eb3b59fd
Jessica Smithers <jessicaesmithers@gmail.com>
2023-02-07 15:49:10 -0500

    Add styling and UI changes.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 const choices = ["rock", "paper", "scissors"];
 let compScore = 0;
 let playerScore = 0;
+let compPic = document.getElementById("compPic");
+let playerPic = document.getElementById("playerPic");
 
 const pscore = document.getElementById("pscore");
 const cscore = document.getElementById("cscore");
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
-let resultsDiv = document.querySelector("#resultsDiv");
+let resultsHeader = document.querySelector("#resultsHeader");
 let results = document.createElement("p");
-resultsDiv.appendChild(results);
+resultsHeader.appendChild(results);
 let winner = document.createElement("p");
-resultsDiv.appendChild(winner);
+results.appendChild(winner);
 pscore.innerHTML = "Player Score: " + playerScore ;
 cscore.innerHTML = "Computer Score: " + compScore;
 
@@ -24,19 +26,12 @@ function getComputerChoice(){
     return choices[randomNum];
 }
 
-function getPlayerChoice(){
-    let playerChoice = prompt("Please choose one: rock, paper, or scissors.").toLowerCase();
-    while(playerChoice != "rock" && playerChoice != "scissors" && playerChoice != "paper"){
-        alert("You did not enter a valid option!");
-        playerChoice = prompt("Please choose one of the following options: rock, paper, or scissors.").toLowerCase();
-    } 
-    return playerChoice;
-}
+
 
 function playRound(user, comp){
     user = this.innerText.toLowerCase();
     comp = getComputerChoice();
-    results.textContent = `User choice: ${user}, Computer choice: ${comp}`;
+    // results.textContent = `User choice: ${user}, Computer choice: ${comp}`;
     switch(true) {
         case user === "rock" && comp === "scissors":
             playerScore++;
@@ -66,25 +61,46 @@ function playRound(user, comp){
             winner.textContent = "Tie game! Both picked " + user + "!";
             break;
     }
+    setTimeout(showPics(user, comp), 10);
     pscore.innerHTML = "Player Score: " + playerScore;
     cscore.innerHTML = "Computer Score: " + compScore;
     setTimeout(endGame, 0);
 }
 
 
-    function endGame(){
-        if(playerScore == 5 || compScore == 5){
-            alert("Game Over!");
-            if(playerScore > compScore){
-                alert("You win!");
-            } else {
-                alert("You lose!");
-            }
-            reset();
+function endGame(){
+    if(playerScore == 5 || compScore == 5){
+        alert("Game Over!");
+        if(playerScore > compScore){
+            alert("You win!");
+        } else {
+            alert("You lose!");
         }
-    
+        reset();
     }
 
+}
+
+function showPics(user, comp){
+    if(user == "rock"){
+        playerPic.setAttribute("src", "rock.jpeg");
+    }
+    if(user == "paper"){
+        playerPic.setAttribute("src", "paper.jpeg");
+    }
+    if(user == "scissors"){
+        playerPic.setAttribute("src", "scissors.webp");
+    }
+    if(comp == "rock"){
+        compPic.setAttribute("src", "rock.jpeg");
+    }
+    if(comp == "paper"){
+        compPic.setAttribute("src", "paper.jpeg");
+    }
+    if(comp == "scissors"){
+        compPic.setAttribute("src", "scissors.webp");
+    }
+}
 
 function reset(){
     playerScore = 0;
@@ -108,3 +124,13 @@ function reset(){
 //         alert("It was a tie!");
 //     }
 //}
+
+
+// function getPlayerChoice(){
+    //     let playerChoice = prompt("Please choose one: rock, paper, or scissors.").toLowerCase();
+    //     while(playerChoice != "rock" && playerChoice != "scissors" && playerChoice != "paper"){
+    //         alert("You did not enter a valid option!");
+    //         playerChoice = prompt("Please choose one of the following options: rock, paper, or scissors.").toLowerCase();
+    //     } 
+    //     return playerChoice;
+    // }
